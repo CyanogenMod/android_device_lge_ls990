908f28ceba6041f962b90fa6414839c127bfb712
Hendrik Hagendorn <git@finnq.de>
2015-11-07 16:19:56 -0800

    ls990: Adjust path for mixer_paths_bcm.xml
    Change-Id: I1e953627564e1e050d7f73c0b790687fbf9bba73

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ $(call inherit-product-if-exists, vendor/lge/ls990/ls990-vendor.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
-    device/lge/g3-common/configs/mixer_paths_bcm.xml:system/etc/mixer_paths.xml
+    device/lge/g3-common/configs/audio/mixer_paths_bcm.xml:system/etc/mixer_paths.xml
 
 # GPS
 PRODUCT_COPY_FILES += \
