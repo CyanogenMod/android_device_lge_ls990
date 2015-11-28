c9d9554a5fc04ba79232a1dd0ff71f81537ddebf
bryan2894 <bryan2894@gmail.com>
2015-11-27 20:12:25 -0500

    ls990: bluetooth: enable ble peripheral mode
    Change-Id: I680156f588b12800de254be73833b5563b62a2d0

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,6 +18,8 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME "LG LS990"
-#define BTA_DISABLE_DELAY 100 /* in milliseconds */
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+
+#define BLE_VND_INCLUDED TRUE
 
 #endif
