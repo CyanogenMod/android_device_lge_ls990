df302bd94f831a03ccee5cb4d5888328a7039a2a
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-01-07 07:45:01 -0600

    ril: rename to ls990RIL and inherit LgeLteRIL
    Change-Id: I050cd8afb0d8f0e5d9ea5532dd01490e9fcb1c63

--- ril/telephony/java/com/android/internal/telephony/ls990RIL.java
+++ ril/telephony/java/com/android/internal/telephony/ls990RIL.java
@@ -31,19 +31,19 @@ import com.android.internal.telephony.uicc.IccCardApplicationStatus;
 import com.android.internal.telephony.uicc.IccCardStatus;
 
 /**
- * Custom Qualcomm RIL for G3
+ * Custom Qualcomm RIL for LS990
  *
  * {@hide}
  */
-public class LgeLteRIL extends RIL implements CommandsInterface {
-    static final String LOG_TAG = "LgeLteRIL";
+public class ls990RIL extends LgeLteRIL implements CommandsInterface {
+    static final String LOG_TAG = "ls990RIL";
 
-    public LgeLteRIL(Context context, int preferredNetworkType,
+    public ls990RIL(Context context, int preferredNetworkType,
             int cdmaSubscription, Integer instanceId) {
         this(context, preferredNetworkType, cdmaSubscription);
     }
 
-    public LgeLteRIL(Context context, int networkMode, int cdmaSubscription) {
+    public ls990RIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
     }
 
