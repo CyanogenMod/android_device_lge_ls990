82e14ba1beb9561862817f3febe06771097bd953
markinb66 <markinb66@gmail.com>
2016-05-13 19:49:27 -0430

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/ls990/bluetooth
 BOARD_HAVE_BLUETOOTH_BCM := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_ls990_defconfig
+TARGET_KERNEL_CONFIG := ls990_defconfig
 TARGET_REQUIRES_BUMP := true
 
 # Partitions
